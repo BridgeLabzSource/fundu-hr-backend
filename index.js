b688e861a407dd5c3672fc82ebd3aef22626b2ec
chandankrishnan <chandan.mandre10@gmail.com>
2016-06-24 15:07:47 +0530

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 var express = require('express')
 app = express(),
-    port = 3011,
+    port = process.env.PORT,
     bodyParser = require('body-parser');
 /**
  * configure 
@@ -15,5 +15,5 @@ app.use(require('./controller/demo'));
  * listen port
  */
 app.listen(port, function() {
-    console.log("Server Runnig");
+    console.log("Server Runnig"+port);
 })
