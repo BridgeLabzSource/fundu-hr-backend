7ef68186c0a6673011c159c88226ae73948838de
chandankrishnan <chandan.mandre10@gmail.com>
2016-06-25 13:37:40 +0530

    modify

--- controller/demo.js
+++ controller/demo.js
@@ -7,13 +7,21 @@ router.get('/', function(req, res) {
 
 router.post('/sendmsg', function(req, res) {
     var message = req.body.message;
-    res.send('your message is :'+message);
+    res.send(message);
 });
 
 router.post('/edit',function(req,res){
 	var edit=req.body.edit;
 	edit='chandan';
 	res.send(edit);
-})
+});
+
+router.post('/inTime',function(req,res){
+
+});
+
+router.post('outTime',function(req,res){
+
+});
 
 module.exports = router;
