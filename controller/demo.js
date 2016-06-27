f264fb5e6e1e5d4b8ffc143bbac8bec294349331
chandankrishnan <you@example.com>
2016-06-27 15:43:53 +0530

    inti

--- controller/demo.js
+++ controller/demo.js
@@ -15,14 +15,7 @@ router.post('/sendmsg', function(req, res) {
 router.post('/edit',function(req,res){
 	var edit=req.body.edit;
 	console.log(edit.sr_id);
-	var data=new db.userModel(edit);
-	data.save(function(err,result){
-		if(err){
-			res.send(err);
-		}else{
-			res.send("respone: "+result);
-		}
-	})
+	res.send(edit.sr_id);
 });
 
 router.post('/inTime',function(req,res){
