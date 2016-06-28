485ec17362756f66ade69c9031b1b22a12418b59
chandankrishnan <chandan.mandre10@gmail.com>
2016-06-28 18:37:36 +0530

    init

--- controller/demo.js
+++ controller/demo.js
@@ -14,7 +14,7 @@ router.post('/sendmsg', function(req, res) {
 
 router.post('/edit',function(req,res){
 	var edit=req.body.sr_id;
-	console.log(req.body.sr_id);
+	console.log(req.body);
 	res.send(edit);
 	// var data=new db.userModel(edit);
 	// data.save(function(err,result){
