cc415e90ab29493aa2f85641e65ad1acf6058377
chandankrishnan <chandan.mandre10@gmail.com>
2016-06-29 11:15:52 +0530

    init1

--- controller/demo.js
+++ controller/demo.js
@@ -14,8 +14,9 @@ router.post('/sendmsg', function(req, res) {
 
 router.post('/edit', function(req, res) {
     var edit = req.body.PAN_card;
-    console.log(" Data:  " + req.body + "\n");
-    var data = req.body.share;
+    console.log(" Data:  " + req.body.share + "\n");
+
+    var data = req.body.share[];
     console.log(" length :"+data.length);
     res.json({ edit: edit });
 });
