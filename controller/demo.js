9bfe9410021a98119b680a28afef5f35dacdcfef
chandankrishnan <chandan.mandre10@gmail.com>
2016-06-29 11:31:18 +0530

    change body

--- controller/demo.js
+++ controller/demo.js
@@ -14,7 +14,7 @@ router.post('/sendmsg', function(req, res) {
 
 router.post('/edit', function(req, res) {
     var edit = req.body.PAN_card;
-    console.log(" Data:  " + req.body + "\n");
+    console.log(" Data:  " + JSON.stringify(req.body) + "\n");
 
     var data = req.body.share;
     console.log(" length :"+data.length);
