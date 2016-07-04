1d5adedf177be61b144a917a43e5e2f8e35a26df
chandankrishnan <chandan.mandre10@gmail.com>
2016-07-04 18:48:03 +0530

    check valid mobile function add

--- model/sms.js
+++ model/sms.js
@@ -64,37 +64,41 @@ sms.prototype.verify = function(data, cb) {
     }
 }
 sms.prototype.save = function(data, cb) {
-    db.userModel.findOne({ "Mobile": data[0][7].Mobile }, function(err, existingUser) {
-        if (!existingUser) {
-            var dbSave = new db.userModel({
-                "srId": data[0][0].sr_id,
-                "empId": data[0][1].empId,
-                "empName": data[0][2].empName,
-                "designation": data[0][3].Designation,
-                "blStartDate": data[0][4].BL_start_date,
-                "startDateAtCompany": data[0][5].start_date_at_company,
-                "endDate": data[0][6].End_Date,
-                "mobile": data[0][7].Mobile,
-                "panCard": data[0][8].PAN_card,
-                "email": data[0][9].email,
-                "dob": data[0][10].DOB,
-                "empContractSigned": data[0][11].Emp_contract_signed,
-                "offerLetter": data[0][12].offer_letter,
-                "empFormCsr": data[0][13].Emp_form_CSR,
-                "originalSubmitted": data[0][14].original_submitted
-            });
-            dbSave.save(function(err, data) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('successfully save');
-                }
-            });
-        } else {
-            console.log('existingUser at ' + i);
-        }
-    });
-    cb(null, "save data");
+    if (common.isMobile(data[0][7].Mobile)) {
+        db.userModel.findOne({ "Mobile": data[0][7].Mobile }, function(err, existingUser) {
+            if (!existingUser) {
+                var dbSave = new db.userModel({
+                    "srId": data[0][0].sr_id,
+                    "empId": data[0][1].empId,
+                    "empName": data[0][2].empName,
+                    "designation": data[0][3].Designation,
+                    "blStartDate": data[0][4].BL_start_date,
+                    "startDateAtCompany": data[0][5].start_date_at_company,
+                    "endDate": data[0][6].End_Date,
+                    "mobile": data[0][7].Mobile,
+                    "panCard": data[0][8].PAN_card,
+                    "email": data[0][9].email,
+                    "dob": data[0][10].DOB,
+                    "empContractSigned": data[0][11].Emp_contract_signed,
+                    "offerLetter": data[0][12].offer_letter,
+                    "empFormCsr": data[0][13].Emp_form_CSR,
+                    "originalSubmitted": data[0][14].original_submitted
+                });
+                dbSave.save(function(err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('successfully save');
+                    }
+                });
+            } else {
+                console.log('existingUser at ' + i);
+            }
+        });
+        cb(null, "save data");
+    } else {
+        cb("Number not proper", null);
+    }
 };
 
 sms.prototype.wit = function(d, cb) {
