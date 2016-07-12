3acb4b3865f931bf388c1a5d8595c78f99b5afaf
chandankrishnan <chandan.mandre10@gmail.com>
2016-07-12 16:06:35 +0530

    add new code

--- model/sms.js
+++ model/sms.js
@@ -139,7 +139,7 @@ sms.prototype.wit = function(d, cb) {
                     var data = r.body,
                         intent = data.entities.intent[0].value,
                         on_off = data.entities.on_off[0].value,
-                        datetime= moment().format("YYYY-MM-DD HH:mm:ss+5:30");
+                        datetime= moment().format("YYYY-MM-DD HH:mm:ss +5:30");
                         console.log(datetime);
                     if (intent == 'Work' || intent == 'office' && on_off == 'on') {
                         var result = {
