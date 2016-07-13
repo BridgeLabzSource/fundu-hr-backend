7ca80b2c8e80b7e52945a223cd668f263d643a78
chandankrishnan <chandan.mandre10@gmail.com>
2016-07-13 16:39:20 +0530

    change

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3008,
     bodyParser = require('body-parser'),
+    cors = require('cors'),
     db = require('./model/connect.js');
 /**
  * configure 
  */
+app.use(cors());
 app.use(bodyParser());
 app.use(require('./controller/index'));
 
