5dae3be6cf1886903c89640d9a1263c362748b3e
chandankrishnan <chandan.mandre10@gmail.com>
2016-07-26 09:39:36 +0530

    comment socket.io

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ app.use(require('./controller/index'));
 /**
  * socket connection on
  */
-io.on('connection', function(socket) {
-    console.log('a user connected to server');
-    socket.on('message', function() {
-        var data = ['I am in office', 'I left from office now', 'I left office yesterday', 'In office', 'Left office', 'I am in', 'I am out'];
-        socket.emit('server ready', data);
-    });
-    socket.on('disconnect', function() {
-        console.log('user disconnected from server');
-    });
-});
+// io.on('connection', function(socket) {
+//     console.log('a user connected to server');
+//     socket.on('message', function() {
+//         var data = ['I am in office', 'I left from office now', 'I left office yesterday', 'In office', 'Left office', 'I am in', 'I am out'];
+//         socket.emit('server ready', data);
+//     });
+//     socket.on('disconnect', function() {
+//         console.log('user disconnected from server');
+//     });
+// });
 
 /**
  * listen port
