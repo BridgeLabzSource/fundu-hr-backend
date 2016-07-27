04d156bf4bb9a2d5921b2d5da41a86b6954fa818
chandankrishnan <chandan.mandre10@gmail.com>
2016-07-27 14:06:30 +0530

    dd

--- model/excel.js
+++ model/excel.js
@@ -3,8 +3,8 @@
  */
 var util = require('util'),
     EventEmitter = require('events').EventEmitter,
-    db = require('../database/db');
-    common=require('../helper/common');
+    db = require('../database/db'),
+    common = require('../helper/common');
 /**
  * @constructor
  */
@@ -28,40 +28,51 @@ excel.prototype.save = function(data, cb) {
         // console.log(i + " inside for loop : " + data[i][7]);
         // var re=JSON.stringify(db.userModel.find({'empId':data[i][1]}));
         // console.log("find   :"+re);
-        var find = data[i][1];
         console.log('before i : ' + i);
 
         var promise = new Promise(function(resolve, reject) {
-            var exist=common.find(data[i][1]);
-            console.log("result from findOne:"+exist+"\n");
-                if (exist==undefined) {
-                    var dd={
-                    'srId': data[i][0],
-                    'empId': data[i][1],
-                    'empName': data[i][2],
-                    'designation': data[i][3],
-                    'blStartDate': data[i][4],
-                    'startDateAtCompany': data[i][5],
-                    'endDate': data[i][6],
-                    'mobile': data[i][7],
-                    'panCard': data[i][8],
-                    'email': data[i][9],
-                    'dob': data[i][10],
-                    'empContractSigned': data[i][11],
-                    'offerLetter': data[i][12],
-                    'empFormCsr': data[i][13],
-                    'originalSubmitted': data[i][14]
+                var exist = common.find(data[i][1]);
+                console.log("result from findOne:" + exist + "\n");
+                db.userModel.findOne({ 'empId': data[i][1] }, function(err, exist) {
+                    if (!exist) {
+                        var dd = new db.userModel({
+                            'srId': data[i][0],
+                            'empId': data[i][1],
+                            'empName': data[i][2],
+                            'designation': data[i][3],
+                            'blStartDate': data[i][4],
+                            'startDateAtCompany': data[i][5],
+                            'endDate': data[i][6],
+                            'mobile': data[i][7],
+                            'panCard': data[i][8],
+                            'email': data[i][9],
+                            'dob': data[i][10],
+                            'empContractSigned': data[i][11],
+                            'offerLetter': data[i][12],
+                            'empFormCsr': data[i][13],
+                            'originalSubmitted': data[i][14]
+                        })
+                        dd.save(function(err, result) {
+                            if (result) {
+                                resolve("resolve")
+                            }
+                        })
+                    } else {
+                        reject(Error("It broke"));
                     }
-                    console.log("resolve"+i+" and "+common.save(dd));
-                    resolve("Stuff worked!");
-                } else {
-                    console.log("reject"+i);
-                    reject(Error("It broke"));
-                }
-            // })
-        });
+                })
+            })
+            // if (exist==undefined) {
+
+        //     console.log("resolve"+i+" and "+common.save(dd));
+        //     resolve("Stuff worked!");
+        // } else {
+        //     console.log("reject"+i);
+        //     reject(Error("It broke"));
+        // }
+        // })
     }
     cb(null, "update");
-};
+}
 
-module.exports = new excel;
+module.exports = new excel();
