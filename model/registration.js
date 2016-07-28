03c485e09be31cbb7ccc5368d3964b9909e1385f
chandankrishnan <chandan.mandre10@gmail.com>
2016-07-28 18:31:07 +0530

    change collection name

--- model/registration.js
+++ model/registration.js
@@ -32,7 +32,7 @@ registration.prototype.register = function(d, cb) {
 
 registration.prototype.login = function(d, cb) {
     if (common.isMobile(d.mobile)) {
-        db.demo.findOne({ 'mobile': d.mobile }, function(err, result) {
+        db.userModel.findOne({ 'mobile': d.mobile }, function(err, result) {
             if (!result || err) {
                 cb('You have not registered', null);
             } else {
